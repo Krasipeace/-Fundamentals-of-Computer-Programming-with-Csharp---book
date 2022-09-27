6fd7d50204f51b6687634ef6367f87f3317d32e2
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-28 00:26:05 +0300

    custom substring

---
+++ CsharpBookBG_Ch_22/1. Custom Substring/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _1._Custom_Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Text: ");
+            string input = Console.ReadLine();
+
+            Console.Write("Enter: index and length of desired Substring: ");
+            int[] commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToArray();
+            string result = input.CustomSubstring(commands[0], commands[1]);
+
+            Console.WriteLine(result);
+        }
+
+    }
+    public static class Extensions
+    {
+        public static string CustomSubstring(this string customStr, int index, int length)
+        {
+            StringBuilder sb = new StringBuilder(customStr);
+
+            return sb.ToString(index, length);
+        }
+    }
+}
