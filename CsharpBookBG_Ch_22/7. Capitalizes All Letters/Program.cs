58bba365b0298f97a4cf56b5c5c18cfb414f57c0
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-28 12:55:03 +0300

    extensions

---
+++ CsharpBookBG_Ch_22/7. Capitalizes All Letters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace _7._Capitalizes_First_Letter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            text.CapitalizeFirstLetter();
+        }
+    }
+    public static class Extensions
+    {
+        public static string CapitalizeFirstLetter(this string customStr)
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+            Console.WriteLine($"{textInfo.ToTitleCase(customStr)}");
+
+            return customStr;
+        }
+    }
+}
